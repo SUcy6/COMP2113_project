1e1e2a386d83b19e76203f4757d61bf9639f29c6
Tina <78636627+SUcy6@users.noreply.github.com>
2021-04-07 00:16:28 +0800

    Add files via upload

---
+++ playing_field.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ncurses.h>
+#include "tetris.h"
+
+using namespace std;
+
+void build_boundary()
+{
+  for(int i = 0;i < 32; i++){
+	for(int j = 0;j < 62; j++){
+	  if( i == 0 || i == 31 || j == 0 || j = 61) {
+		field[i][j]=1;
+	  }
+	  else{
+		field[i][j]=0;
+      }
+	}
+  }
+}
+
+WINDOW * initial_playwin(int Height, int Width, int a, int b)
+{
+  // use newwin() to build a new window
+  // newwin() would return a pointer
+  WINDOW * win1;
+  win1 = newwin(Height, Width, a, b);
+    
+  // the window built by newwin() is invisible
+  // use box() to add a solid frame
+  box(win, '#', '#');
+
+  wrefresh(win);  // update the playing window
+
+  return win;
+}
+
+
---
+++ tetris_piece.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <ncurses.h>
+#include <cstdlib>      // for calling srand(), rand()
+#include <ctime>        // for calling time() 
+#include "tetris.h"
+
+using namespace std;
+
+void build_tetris(int &choice_s, int &choice_p, int shape[][4], int &H, int &W)
+{ 
+  // build a 4x4 empty matrix to initialize a tetris piece
+  for(int i = 0;i < 4; i++){
+    for(int j = 0; j < 4; j++){
+	  shape[i][j]=0;
+    }
+  }
+
+  switch(choice_s)
+  {
+	case 1:	// '####' shape
+	  H = 1;
+	  W = 4;	
+      for (int i = 0; i < 4; i++){
+          shape[0][i] = 1;
+      }
+	  break;
+
+	case 2: 
+    //         '#  ' shape
+    //          ###   
+	  H = 2;
+	  W = 3;
+	  shape[0][0]=1;
+      for (int i = 0; i < 3; i++){
+          shape[1][i] = 1;
+      }
+	  break;
+
+	case 3:
+    //         '  #' shape
+    //          ### 
+	  H = 2;
+	  W = 3;	
+	  shape[0][2]=1;
+	  for (int i = 0; i < 3; i++){
+          shape[1][i] = 1;
+      }
+	  break;
+
+	case 4:
+    //        '## ' shape
+    //          ##
+	  H = 2;
+	  W = 3;
+	  shape[0][0] = shape[0][1] = shape[1][1] = shape[1][2] = 1;
+	  break;
+    
+    case 5:
+    //        ' ##' shape
+    //         ##
+	  H = 2;
+	  W = 3;
+	  shape[0][1] = shape[0][2] = shape[1][0] = shape[1][1] = 1;
+	  break;
+
+	case 6:	
+    //        '##' shape
+    //         ##
+	  H = 2;
+	  W = 2;
+	  shape[0][0] = shape[0][1] = shape[1][0] = shape[1][1] = 1;
+	  break;
+
+	case 7:	
+    //        ' # ' shape
+    //         ###
+	  H = 2;
+	  W = 3;
+	  shape[0][1]=1;
+	  for (int i = 0; i < 3; i++){
+          shape[1][i] = 1;
+      }
+	  break;
+  }
+
+  // set falling tetris position
+  switch(choice_p)
+  {
+    case 1:
+      x = 30;
+      y = 1;
+    
+    case 2:
+      x = 30;
+      y = 30;
+    
+    case 3:
+      x = 1;
+      y = 15;
+    
+    case 4:
+      x = 60;
+      y = 15;
+  }
+  
+  // ***
+  // if the new falling tetris touch 
+  if(check_collision(shape))    
+	game_over=true;	
+}
+
+void initial_tetris(tetris * &fp)
+{ 
+  // get random shape no. and position no. 
+  srand(time(NULL));
+  (*fp).choice_s = rand()%7 + 1;
+  (*fp).choice_p = rand()%4 + 1;
+  
+  // build a tetris piece
+  build_tetris((*fp).choice_s, (*fp).choice_p, (*fp).shape, (*fp).H, (*fp).W);
+
+  // ***
+  mvwprintw(score_win, hint_win_height/2, hint_win_width/2-2, "%d", score);
+  wrefresh(score_win);
+}
