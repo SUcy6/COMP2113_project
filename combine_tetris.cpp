902fcf27d969c52858eb8025b927253b942f6895
Amelia <78427693+amarantine-0622@users.noreply.github.com>
2021-04-09 00:42:34 +0800

    Update combine_tetris.cpp

--- combine_tetris.cpp
+++ combine_tetris.cpp
@@ -1,17 +1,15 @@
-#include <iostream>
 #include "tetris.h"
 using namespace std;
 
 // combine falling tetris and middle tetris to be the new middle tetris
-void combine_tetris ( int ** & middle_tetris, int ** falling_tetris, const int falling_height, const int falling_width,
-  const int middle_height, const int middle_width, const int middle_coordinates[2], const int falling_coordinates[2] )
+void combine_tetris ( int ** & middle_tetris, tetris fp, tetris mp )
 {
   int upper_bound, lower_bound, left_bound, right_bound;
 
-  upper_bound = min (falling_coordinates[0], middle_coordinates[0]);
-  lower_bound = max (falling_coordinates[0] + falling_height - 1, middle_coordinates[0] + middle_height - 1);
-  left_bound = min (falling_coordinates[1], middle_coordinates[1]);
-  right_bound = max (falling_coordinates[1] + falling_width - 1, middle_coordinates[1] + middle_width - 1);
+  upper_bound = min (fp.y, mp.y);
+  lower_bound = max (fp.y + fp.H - 1, mp.y + mp.H - 1);
+  left_bound = min (fp.x, mp.x);
+  right_bound = max (fp.x + fp.W - 1, mp.x + mp.W - 1);
 
   // combined_tetris declaration
   int ** combined_tetris = new int * [lower_bound - upper_bound + 1];
@@ -20,18 +18,18 @@ void combine_tetris ( int ** & middle_tetris, int ** falling_tetris, const int f
   }
 
   // combined_tetris initialization
- for (int i = 0; i < falling_height; i++) {
-   for (int j = 0; j < falling_width; j++) {
-     if (falling_tetris[i][j] == 1) {
-       combined_tetris[i - upper_bound + falling_coordinates[0]][j - left_bound + falling_coordinates[1]] = 1;
+ for (int i = 0; i < 4; i++) {
+   for (int j = 0; j < 4; j++) {
+     if (fp.shape[i][j] == 1) {
+       combined_tetris[i - upper_bound + fp.y][j - left_bound + fp.x] = 1;
      }
    }
  }
 
- for (int i = 0; i < middle_height; i++) {
-   for (int j = 0; j < middle_width; j++) {
+ for (int i = 0; i < mp.H; i++) {
+   for (int j = 0; j < mp.W; j++) {
      if (middle_tetris[i][j] == 1) {
-       combined_tetris[i - upper_bound + middle_coordinates[0]][j - left_bound + middle_coordinates[1]] = 1;
+       combined_tetris[i - upper_bound + mp.y]][j - left_bound + mp.x] = 1;
      }
    }
  }
