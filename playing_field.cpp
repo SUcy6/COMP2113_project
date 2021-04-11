bc329eefc9cbaba31f8e6379c13090b34d743ffa
Tina <78636627+SUcy6@users.noreply.github.com>
2021-04-11 12:46:19 +0800

    Update playing_field.cpp

--- playing_field.cpp
+++ playing_field.cpp
@@ -8,7 +8,7 @@ void build_boundary(int field[][62])
 {
   for(int i = 0;i < 32; i++){
 	for(int j = 0;j < 62; j++){
-	  if( i == 0 || i == 31 || j == 0 || j = 61) {
+	  if( i == 0 || i == 31 || j == 0 || j == 61) {
 		field[i][j]=1;
 	  }
 	  else{
@@ -27,11 +27,11 @@ WINDOW * initial_playwin(int Height, int Width, int a, int b)
     
   // the window built by newwin() is invisible
   // use box() to add a solid frame
-  box(win, '#', '#');
+  box(win1, '#', '#');
 
-  wrefresh(win);  // update the playing window
+  wrefresh(win1);  // update the playing window
 
-  return win;
+  return win1;
 }
 
 
