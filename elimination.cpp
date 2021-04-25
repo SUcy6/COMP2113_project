4c4f8f4478ae44af971d2b0eef6e68abaa5bfef9
Amelia <78427693+amarantine-0622@users.noreply.github.com>
2021-04-25 21:36:37 +0800

    Update elimination.cpp

--- elimination.cpp
+++ elimination.cpp
@@ -2,96 +2,156 @@
 #include "tetris.h"
 using namespace std;
 
-void elimination ( mtetris & mp, int ** & middle_tetris, int & score )
+void elimination ( mtetris & mp, int ** middle_tetris, int & score, WINDOW * main_win )
 {
-  class Elimination {
-  public:
-    int x;
-    int y;
-    int side;
-  };
-
   vector<Elimination> eliminations;
 
+  // boudaries before elimination
   int upper_bound = mp.y;
   int lower_bound = mp.y + mp.H - 1;
   int left_bound = mp.x;
   int right_bound = mp.x + mp.W - 1;
 
-  // check_tetris declaration: 0 if no tile or checked; 1 for unchecked tile
-  int ** check_tetris = new int * [mp.H];
-  for (int i = 0; i < mp.H; i++) {
-    check_tetris[i] = new int [mp.W];
-  }
+  // check initialization: 0 if no tile or checked; 1 for unchecked tile
+  int * check = new int [mp.H * mp.W];
 
-  // check_tetris initialization
   for (int i = 0; i < mp.H; i++) {
     for (int j = 0; j < mp.W; j++) {
       if (middle_tetris[i][j] == 1) {
-        check_tetris[i][j] = 1;
+        check [i * mp.W + j] = 1;
       }
       else {
-        check_tetris[i][j] = 0;
+        check [i * mp.W + j] = 0;
       }
     }
   }
 
   // middle tetris larger than 3x3
   if ( mp.W >= 3 && mp.H >= 3 ) {
-    int i = upper_bound, j = left_bound;
-    while ( check_tetris[i][j] == 1 && i < lower_bound - 1 && j < right_bound - 1 ) {
-      if ( middle_tetris [i][j] == 1 && middle_tetris [i][j + 1] == 1 && middle_tetris [i][j + 2] == 1 &&
-      middle_tetris [i + 1][j] == 1 && middle_tetris [i + 1][j + 1] == 1 && middle_tetris [i + 1][j + 2] == 1 &&
-    middle_tetris [i + 2][j] == 1 && middle_tetris [i + 2][j + 1] == 1 && middle_tetris [i + 2][j + 2] == 1 ) { // 3x3 tetris existing
-        Elimination e;
-        e.x = i;
-        e.y = j;
-        int current_side = 3;
-        bool flag = 1;
-        // find the biggest square
-        while ( i + current_side <= lower_bound && j + current_side <= mp.W ) {
-          // check larger square
-          for ( int k = 0; k < current_side + 1; k++ ) {
-            if ( middle_tetris [i + current_side] [j + k] != 1 ) {
-              flag = 0;
-              break;
+
+    for (int i = upper_bound - mp.y; i < mp.H - 2; i++) {
+      for (int j = left_bound - mp.x; j < mp.W - 2; j++) {
+
+        if (check [i * mp.W + j] == 1) {
+
+          if ( middle_tetris [i][j] == 1 && middle_tetris [i][j + 1] == 1 && middle_tetris [i][j + 2] == 1 &&
+          middle_tetris [i + 1][j] == 1 && middle_tetris [i + 1][j + 1] == 1 && middle_tetris [i + 1][j + 2] == 1 &&
+        middle_tetris [i + 2][j] == 1 && middle_tetris [i + 2][j + 1] == 1 && middle_tetris [i + 2][j + 2] == 1 ) { // 3x3 tetris existing
+
+            Elimination e;
+            e.y = i; // relative coordinates
+            e.x = j;
+            int current_side = 3;
+
+            bool flag = 1;
+
+            // find the biggest square
+            while ( i + current_side < mp.H && j + current_side < mp.W ) {
+              // check larger square
+              for ( int k = 0; k < current_side + 1; k++ ) {
+                if ( middle_tetris [i + current_side] [j + k] != 1 ) {
+                  flag = 0;
+                  break;
+                }
+              }
+              for ( int k = 0; k < current_side + 1; k++ ) {
+                if ( middle_tetris [i + k] [j + current_side] != 1 ) {
+                  flag = 0;
+                  break;
+                }
+              }
+              // end the loop if fails
+              if (flag) {
+                current_side += 1;
+              }
+              else {
+                break;
+              }
+              flag = 1;
             }
-          }
-          for ( int k = 0; k < current_side + 1; k++ ) {
-            if ( middle_tetris [i + k] [j + current_side] != 1 ) {
-              flag = 0;
-              break;
+
+            // record the elimination square
+            e.side = current_side;
+            eliminations.push_back(e);
+
+            // label as checked
+            for ( int x = i; x < i + current_side - 1; x++ ) {
+              for (int y = j; y < j + current_side - 1; y++ ) {
+                check [x * mp.W + y] = 0;
+              }
             }
-          }
-          // end the loop if fails
-          if ( flag ) {
-            current_side += 1;
-          }
-          else {
-            break;
-          }
-          flag = 1;
-        }
-        e.side = current_side;
-        eliminations.push_back(e);
-        for ( int x = i; x < i + current_side - 1; x++ ) {
-          for (int y = j; y < j + current_side - 1; y++ ) {
-            check_tetris [x][y] = 0;
           }
         }
       }
     }
-  }
 
-  // elimination
-  for ( int i = 0; i < eliminations.size(); i++ ) {
-    for ( int x = eliminations[i].x; x < eliminations[i].x + eliminations[i].side; x++ ) {
-      for ( int y = eliminations[i].y; y < eliminations[i].y + eliminations[i].side; y++ ) {
-        middle_tetris [x][y] = 0;
-        score += 10;
+    // elimination
+    for ( int i = 0; i < eliminations.size(); i++ ) {
+      for ( int x = eliminations[i].y; x < eliminations[i].y + eliminations[i].side; x++ ) {
+        for ( int y = eliminations[i].x; y < eliminations[i].x + eliminations[i].side; y++ ) {
+          if (middle_tetris[x][y] == 1) {
+            middle_tetris[x][y] = 0;
+            mvwaddch(main_win, (mp).y+x, (mp).x+y, ' ');
+            score += 10;
+          }
+        }
       }
     }
-  }
 
-  delete[] check_tetris;
+    wrefresh(main_win);
+
+    upper_bound = mp.y + mp.H - 1;
+    lower_bound = mp.y;
+    left_bound = mp.x + mp.W - 1;
+    right_bound = mp.x;
+
+    // update mtetris mp
+    for (int i = 0; i < mp.H; i++) {
+      for (int j = 0; j < mp.W; j++) {
+        if (middle_tetris[i][j] == 1) {
+          if (i + mp.y < upper_bound) {
+            upper_bound = i + mp.y;
+          }
+          if (i + mp.y > lower_bound) {
+            lower_bound = i + mp.y;
+          }
+          if (j + mp.x < left_bound) {
+            left_bound = j + mp.x;
+          }
+          if (j + mp.x > right_bound) {
+            right_bound = j + mp.x;
+          }
+        }
+      }
+    }
+
+    // store new mp information
+    int temp_x = left_bound;
+    int temp_y = upper_bound;
+    int temp_W = right_bound - left_bound + 1;
+    int temp_H = lower_bound - upper_bound + 1;
+
+    // new_tetris declaration
+    int ** eliminated_tetris = new int * [temp_H];
+    for (int i = 0; i < temp_H; i++) {
+      eliminated_tetris[i] = new int[temp_W];
+    }
+
+    // new_tetris initialization
+    for (int i = 0; i < temp_H; i++) {
+      for (int j = 0; j < temp_W; j++) {
+        //cout << i << ' ' << j << ' ' << i + temp_y - mp.y << ' ' << j + temp_x - mp.x << endl;
+        //cout << new_tetris[i][j] << ' ' << middle_tetris[i + temp_y - mp.y][j + temp_x - mp.x] << endl;
+        eliminated_tetris[i][j] = middle_tetris[i + temp_y - mp.y][j + temp_x - mp.x];
+      }
+    }
+
+    middle_tetris = eliminated_tetris;
+
+    // update new mp info
+    mp.x = temp_x;
+    mp.y = temp_y;
+    mp.H = temp_H;
+    mp.W = temp_W;
+  }
 }
